f169747476e4d08b76fd92c12582fb438c05d9c1
Kamil Graczyk <49203388+St0n3k@users.noreply.github.com>
2023-03-23 11:42:55 +0100

    Initial commit

---
+++ src/main/java/pl/it/p/lodz/pl/zzpj/WordIoApplication.java
@@ -0,0 +1,13 @@
+package pl.it.p.lodz.pl.zzpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordIoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordIoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/it/p/lodz/pl/zzpj/WordIoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.it.p.lodz.pl.zzpj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordIoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

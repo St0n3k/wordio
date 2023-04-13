4a659ab82c64e1693bd9b31fbed5e23a36047d7a
Kamil Graczyk <49203388+St0n3k@users.noreply.github.com>
2023-04-13 14:15:59 +0200

    Annotated doFilterInternal method with @NonNull

--- backend/src/main/java/pl/lodz/p/it/zzpj/config/security/JwtFilter.java
+++ backend/src/main/java/pl/lodz/p/it/zzpj/config/security/JwtFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -29,7 +30,8 @@ public class JwtFilter extends OncePerRequestFilter {
 
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain)
         throws ServletException, IOException {
         String jwt = jwtProvider.getToken(request);
 

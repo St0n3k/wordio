5f3937c34108cc7d0376771b015a3d521fce27f4
Kamil Graczyk <49203388+St0n3k@users.noreply.github.com>
2023-04-15 18:52:24 +0200

    Configured initial-data import for IT tests.

--- backend/src/test/java/pl/lodz/p/it/zzpj/TestContainersSetup.java
+++ backend/src/test/java/pl/lodz/p/it/zzpj/TestContainersSetup.java
@@ -1,13 +1,21 @@
 package pl.lodz.p.it.zzpj;
 
+import static java.net.HttpURLConnection.HTTP_OK;
+import static java.net.HttpURLConnection.HTTP_UNAUTHORIZED;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.HttpWaitStrategy;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.utility.MountableFile;
+
+import java.io.IOException;
+import java.time.Duration;
 
 @SpringBootTest
 @Testcontainers
@@ -24,6 +32,11 @@ public abstract class TestContainersSetup {
             super(DockerImageName.parse("mongo:6.0.5"));
             this.withEnv("MONGO_INITDB_ROOT_USERNAME", "word-io")
                 .withEnv("MONGO_INITDB_ROOT_PASSWORD", "word-io-pwd")
+                .withCopyFileToContainer(MountableFile.forClasspathResource("accounts.json"), "/accounts.json")
+                .waitingFor(new HttpWaitStrategy()
+                    .forPort(27017)
+                    .forStatusCodeMatching(response -> response == HTTP_OK || response == HTTP_UNAUTHORIZED)
+                    .withStartupTimeout(Duration.ofMinutes(2)))
                 .withExposedPorts(27017)
                 .withReuse(true);
         }
@@ -34,9 +47,17 @@ public abstract class TestContainersSetup {
     }
 
     @BeforeAll
-    public static void setup() {
+    public static void setup() throws IOException, InterruptedException {
         mongoContainer = new MongoDBContainer();
         mongoContainer.start();
+        mongoContainer.execInContainer(
+            "/usr/bin/mongoimport",
+            "--collection=account",
+            "--db=wordio",
+            "--username=word-io",
+            "--password=word-io-pwd",
+            "--authenticationDatabase=admin",
+            "./accounts.json");
     }
 
     @AfterAll

ae4c4a272b1e81826ed66e782415a3c468ffae18
Kamil Graczyk <49203388+St0n3k@users.noreply.github.com>
2023-04-27 00:53:29 +0200

    Implemented logging in and registering with alerts.

---
+++ frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { AlertService } from '../../services/alert.service';
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+    loginForm = new FormGroup({
+        username: new FormControl('', [Validators.required]), //Validators.email]),
+        password: new FormControl('', [Validators.required])
+    });
+
+    constructor(
+        private authService: AuthService,
+        private router: Router,
+        private alertService: AlertService
+    ) {}
+
+    get passwordInput() {
+        return this.loginForm.get('password');
+    }
+
+    onSubmit() {
+        if (this.loginForm.valid) {
+            const username = this.loginForm.getRawValue().username;
+            const password = this.loginForm.getRawValue().password;
+
+            this.authService
+                .login(username!.toString(), password!.toString())
+                .subscribe(
+                    (result) => {
+                        if (result.status == 200) {
+                            this.authService.saveUserData(result);
+                            this.authService.authenticated.next(true);
+                            this.router.navigate(['/']);
+                        }
+                    },
+                    (error) => {
+                        this.authService.clearUserData();
+                        this.authService.authenticated.next(false);
+                        this.alertService.showAlert(
+                            'error',
+                            error.error.message
+                        );
+                        this.clearPassword();
+                    }
+                );
+        }
+    }
+
+    clearPassword() {
+        this.loginForm.get('password')?.reset();
+    }
+}
